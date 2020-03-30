bde0e28b6437fbfe0a3cc579543165e1672ab799
Vladislav Bambuch <vladislav.bambuch@kiwi.com>
2020-03-30 18:01:08 +0200

    separate grid layouts

--- crypto-scraper/src/resources/views/layouts/main.blade.php
+++ crypto-scraper/src/resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
         <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
-        <div class="corvid-grid-container">
+        <div class="main-grid-container">
         
             {{-- navbar --}}
             <div class="navbar bg-primary">
--- crypto-scraper/src/resources/views/scheduler.blade.php
+++ crypto-scraper/src/resources/views/scheduler.blade.php
@@ -1,34 +1,36 @@
 @extends('layouts.main')
   
 @section('body')
-    <div class="container d-flex">
-        <div class="col-12 align-self-center text-center">
-            <h3>Here you can schedule scraping jobs.</h3>
+    <div class="scheduler-grid-container">
+        <div class="container d-flex">
+            <div class="col-12 align-self-center text-center">
+                <h3>Here you can schedule scraping jobs.</h3>
+            </div>
         </div>
-    </div>
+        
+        {{-- result data --}}
+        <div class="container-lg">
     
-    {{-- result data --}}
-    <div class="container-lg">
-
-        {{-- table desctiption --}}    
-        <p class="text-secondary mb-2 ml-2">Bitcointalk jobs</p>
-
-        {{-- fixed result head --}}
-        <table class="table table-light hidden-overflow-table">
-            <thead>
-                <tr>
-                    <th class="col-medium">Name</th>
-                    <th class="col-large">Description</th>
-                    <th class="col-small text-center">Frequency</th>
-                    <th class="col-small text-center">Starting</th>
-                    <th class="col-small text-center">Submit</th>
-                </tr>
-            </thead>
-        </table>
-
-        {{-- scrollable result body --}}
-        <div class="table-limit-50">
-            @each('components.task', $tasks, 'task')
+            {{-- table desctiption --}}    
+            <p class="text-secondary mb-2 ml-2">Bitcointalk jobs</p>
+    
+            {{-- fixed result head --}}
+            <table class="table table-light hidden-overflow-table">
+                <thead>
+                    <tr>
+                        <th class="col-medium">Name</th>
+                        <th class="col-large">Description</th>
+                        <th class="col-small text-center">Frequency</th>
+                        <th class="col-small text-center">Starting</th>
+                        <th class="col-small text-center">Submit</th>
+                    </tr>
+                </thead>
+            </table>
+    
+            {{-- scrollable result body --}}
+            <div class="table-limit-50">
+                @each('components.task', $tasks, 'task')
+            </div>
         </div>
     </div>
 @endsection
