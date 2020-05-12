5eefcaea4bcd42155ea83e9a9fc25e316f2a31c7
vbambuch <v.bambuch@gmail.com>
2020-05-12 12:16:49 +0200

    note deprecated methods

--- crypto-corvid/src/app/Console/Base/Common/CryptoParser.php
+++ crypto-corvid/src/app/Console/Base/Common/CryptoParser.php
@@ -41,11 +41,81 @@ class CryptoParser extends Command {
 
         $this->print && $this->url && $this->printParsingPage($this->url);
     }
+
+
+    private function makeRequest(string $url): array {
+        $browser = new Client(HttpClient::create(['proxy' => 'proxy:5566']));
+        // delete history to prevent running out of memory
+//        $browser->restart();
+        // to prevent traffic overloading
+        usleep(intval(env('SCRAPER_TIMEOUT', 2000)));
+        $request = $browser->request('GET', $url);
+        $response = $browser->getResponse();
+        $status = $response->getStatusCode();
+        if ($status != 200) {
+            $this->line("<fg=red>Page " . $url . " responded with status " . $status . "!</>");
+            $this->warningGraylog("Failed to scrape page", $url, ["status" => $status]);
+        }
+
+        return [$request, $response];
+    }
+
+    protected function getPageCrawler(string $url): Crawler {
+        [$request] = $this->makeRequest($url);
+        return $request;
+    }
+
+    protected function getPageContent(string $url): string {
+        [, $response] = $this->makeRequest($url);
+        return $response->getContent();
+    }
+
+    protected function getFullHost(string $url = null): string {
+        $parsedUrl = parse_url($url ?? $this->url);
+        return $parsedUrl["scheme"] . "://" . $parsedUrl["host"];
+    }
+
+    private function graylogChannel(): LoggerInterface {
+        return Log::channel('gelf');
+    }
+
+    private function getGraylogAttrs(array $context, string $logType, $payload): array {
+        return array_merge($context, ["serviceName" => $this->serviceName, "logType" => $logType, "payload" => $payload]);
+    }
+
+    public function infoGraylog(string $message, string $logType, $payload = null, array $context = []) {
+        $attrs = $this->getGraylogAttrs($context, $logType, $payload);
+        $this->graylogChannel()->info($message, $attrs);
+        $this->info($message);
+    }
+
+    public function errorGraylog(string $message, Exception $e = null, array $context = []) {
+        $attrs = $this->getGraylogAttrs($context, GraylogTypes::ERROR, "");
+        $this->graylogChannel()->error($message, $attrs);
+        report($e);
+        $this->error($message);
+        if ($e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    public function warningGraylog(string $message, $payload = null, array $context = []) {
+        $attrs = $this->getGraylogAttrs($context, GraylogTypes::WARN, $payload);
+        $this->graylogChannel()->warning($message, $attrs);
+        $this->warn($message);
+    }
+
+    public function debugGraylog(string $message, string $logType, $payload = null, array $context = []) {
+        $attrs = $this->getGraylogAttrs($context, $logType, $payload);
+        $this->graylogChannel()->debug($message, $attrs);
+        $this->info($message);
+    }
     
     /**
      * Print a string when verbose > 1.
      * Verbose output printing management.
      *
+     * @deprecated 
      * @param string $text Text to print
      * @return void
      */
@@ -59,6 +129,7 @@ class CryptoParser extends Command {
      * Print a string when verbose > 2.
      * Verbose output printing management.
      *
+     * @deprecated
      * @param string $text Text to print
      * @return void
      */
@@ -72,6 +143,7 @@ class CryptoParser extends Command {
      * Print a string when verbose > 2.
      * Verbose output printing management.
      *
+     * @deprecated
      * @param string $text Text to print
      * @return void
      */
@@ -80,15 +152,24 @@ class CryptoParser extends Command {
             print($text);
         }
     }
-    
+
+    /**
+     * @deprecated
+     */
     protected function printRedLine($text) {
         $this->line('<fg=red>' . $text . '</>');
     }
-    
+
+    /**
+     * @deprecated
+     */
     protected function printCyanLine($text) {
         $this->line('<fg=cyan>' . $text . '</>');
     }
 
+    /**
+     * @deprecated
+     */
     protected function saveParsedData(string $dateTime, ParsedAddress ...$parsedAddresses) {
         if (!empty($parsedAddresses)) {
             $progressBar = $this->output->createProgressBar(count($parsedAddresses));
@@ -109,15 +190,24 @@ class CryptoParser extends Command {
             $this->printDetail("- no data to insert.\n");
         }
     }
-    
+
+    /**
+     * @deprecated
+     */
     protected function printParsingPage(string $url) {
         $this->line("<fg=cyan>Parsing page: " . $url ."</>");
     }
-    
+
+    /**
+     * @deprecated
+     */
     protected function printVerbosity() {
         $this->line("<fg=green>Starting with output verbosity: ". $this->verbose .".</>");
     }
-    
+
+    /**
+     * @deprecated
+     */
     protected function getDOMBody(string $url): ?StreamInterface {
         try {
             return $this->browser
@@ -129,73 +219,5 @@ class CryptoParser extends Command {
             $this->errorGraylog("Failed to get DOMBody", $exception, ["url" => $url]);
             return null;
         }
-    }
-    
-    private function makeRequest(string $url): array {
-        $browser = new Client(HttpClient::create(['proxy' => 'proxy:5566']));
-        // delete history to prevent running out of memory
-//        $browser->restart();
-        // to prevent traffic overloading
-        usleep(intval(env('SCRAPER_TIMEOUT', 2000)));
-        $request = $browser->request('GET', $url);
-        $response = $browser->getResponse();
-        $status = $response->getStatusCode();
-        if ($status != 200) {
-            $this->line("<fg=red>Page " . $url . " responded with status " . $status . "!</>");
-            $this->warningGraylog("Failed to scrape page", $url, ["status" => $status]);
-        }
-        
-        return [$request, $response];
-    }
-
-    protected function getPageCrawler(string $url): Crawler {
-        [$request] = $this->makeRequest($url);
-        return $request;
-    }
-    
-    protected function getPageContent(string $url): string {
-        [, $response] = $this->makeRequest($url);
-        return $response->getContent();
-    }
-    
-    protected function getFullHost(string $url = null): string {
-        $parsedUrl = parse_url($url ?? $this->url);
-        return $parsedUrl["scheme"] . "://" . $parsedUrl["host"];
-    }
-    
-    private function graylogChannel(): LoggerInterface {
-        return Log::channel('gelf');
-    }
-    
-    private function getGraylogAttrs(array $context, string $logType, $payload): array {
-        return array_merge($context, ["serviceName" => $this->serviceName, "logType" => $logType, "payload" => $payload]);
-    }
-
-    public function infoGraylog(string $message, string $logType, $payload = null, array $context = []) {
-        $attrs = $this->getGraylogAttrs($context, $logType, $payload);
-        $this->graylogChannel()->info($message, $attrs);
-        $this->info($message);
-    }
-    
-    public function errorGraylog(string $message, Exception $e = null, array $context = []) {
-        $attrs = $this->getGraylogAttrs($context, GraylogTypes::ERROR, "");
-        $this->graylogChannel()->error($message, $attrs);
-        report($e);
-        $this->error($message);
-        if ($e) {
-            $this->error($e->getMessage());
-        }
-    }
-
-    public function warningGraylog(string $message, $payload = null, array $context = []) {
-        $attrs = $this->getGraylogAttrs($context, GraylogTypes::WARN, $payload);
-        $this->graylogChannel()->warning($message, $attrs);
-        $this->warn($message);
-    }
-    
-    public function debugGraylog(string $message, string $logType, $payload = null, array $context = []) {
-        $attrs = $this->getGraylogAttrs($context, $logType, $payload);
-        $this->graylogChannel()->debug($message, $attrs);
-        $this->info($message);
     }
 }
