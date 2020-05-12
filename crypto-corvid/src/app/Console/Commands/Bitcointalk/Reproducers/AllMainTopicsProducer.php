279721632356c9ad3d6fc96d5ae73235188e340d
vbambuch <v.bambuch@gmail.com>
2020-05-12 17:58:46 +0200

    AllMainTopicsProducer simplified

--- crypto-corvid/src/app/Console/Commands/Bitcointalk/Reproducers/AllMainTopicsProducer.php
+++ crypto-corvid/src/app/Console/Commands/Bitcointalk/Reproducers/AllMainTopicsProducer.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace App\Console\Commands\Bitcointalk\Reproducers;
 
-use App\Console\Base\Bitcointalk\KafkaProducer;
 use App\Console\Constants\Bitcointalk\BitcointalkCommands;
-use App\Console\Constants\Bitcointalk\BitcointalkKafka;
-use App\Models\Kafka\UrlMessage;
-use App\Models\Pg\Bitcointalk\BitcointalkModel;
 use App\Models\Pg\Bitcointalk\MainTopic;
+use Illuminate\Console\Command;
 
-class AllMainTopicsProducer extends KafkaProducer {
+class AllMainTopicsProducer extends Command {
 
     protected $signature = BitcointalkCommands::ALL_MAIN_TOPICS_PRODUCER .' {verbose=1} {--force} {dateTime?}';
     
@@ -21,26 +18,11 @@ class AllMainTopicsProducer extends KafkaProducer {
     }
 
     public function handle() {
-        $this->outputTopic = BitcointalkKafka::MAIN_TOPICS_TOPIC;
-        $this->serviceName = BitcointalkCommands::ALL_MAIN_TOPICS_PRODUCER;
-        $this->tableName = MainTopic::class;
-
-        parent::handle();
 
         /**
          * All main topics has to be re-scraped to find new topic pages.
          */
         MainTopic::setParsedToAll(false);
-        $unBoardPages = MainTopic::getAllUnParsed();
-        foreach ($unBoardPages as $unBoardPage) {
-            $parentUrl = $unBoardPage->getAttribute(BitcointalkModel::COL_PARENT_URL);
-            $url = $unBoardPage->getAttribute(BitcointalkModel::COL_URL);
-            $urlMessage = new UrlMessage($parentUrl, $url, false);
-            $this->kafkaProduce($urlMessage->encodeData());
-        }
-    }
-    
-    protected function loadDataFromUrl(string $url): array {
-        return [];
+        $this->call(BitcointalkCommands::UN_MAIN_TOPICS_PRODUCER);
     }
 }
